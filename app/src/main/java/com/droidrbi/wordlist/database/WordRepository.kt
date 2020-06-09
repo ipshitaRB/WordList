58874751384d8c2b8ddda25b96920568d36c94c0
ipshitaRB <ipshitarburman@gmail.com>
2020-06-08 23:11:47 -0400

    Create Word ViewModel

--- app/src/main/java/com/droidrbi/wordlist/database/WordRepository.kt
+++ app/src/main/java/com/droidrbi/wordlist/database/WordRepository.kt
@@ -1,4 +1,14 @@
 package com.droidrbi.wordlist.database
 
-class WordRepository {
+import androidx.lifecycle.LiveData
+import com.droidrbi.wordlist.model.Word
+
+class WordRepository(private val wordDao: WordDao) {
+    // Room executes all queries on a separate thread.
+    // Observed LiveData will notify the observer when the data has changed.
+    val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
+
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
 }
---
+++ app/src/main/java/com/droidrbi/wordlist/model/WordViewModel.kt
@@ -0,0 +1,7 @@
+package com.droidrbi.wordlist.model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class WordViewModel(application: Application): AndroidViewModel(application) {
+}
