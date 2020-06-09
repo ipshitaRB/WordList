776bbe9387d43c28a45f8523f13af254d57c83f2
ipshitaRB <ipshitarburman@gmail.com>
2020-06-08 23:28:20 -0400

    Implement Word ViewModel

--- app/src/main/java/com/droidrbi/wordlist/model/WordViewModel.kt
+++ app/src/main/java/com/droidrbi/wordlist/model/WordViewModel.kt
@@ -2,6 +2,27 @@ package com.droidrbi.wordlist.model
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.droidrbi.wordlist.database.WordRepository
+import com.droidrbi.wordlist.database.WordRoomDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class WordViewModel(application: Application): AndroidViewModel(application) {
+
+    private val repository: WordRepository
+
+    val allWords: LiveData<List<Word>>
+
+    init {
+        val wordsDao = WordRoomDatabase.getDatabase(application).wordDao()
+        repository = WordRepository(wordsDao)
+        allWords = repository.allWords
+    }
+
+    fun insert(word: Word) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(word)
+    }
 }
