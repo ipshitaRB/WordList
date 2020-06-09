efb5386b2fafedfe62bce615675ddc3ed2ef5df3
ipshitaRB <ipshitarburman@gmail.com>
2020-06-08 22:57:06 -0400

    Implement Room Database

--- app/src/main/java/com/droidrbi/wordlist/database/WordRoomDatabase.kt
+++ app/src/main/java/com/droidrbi/wordlist/database/WordRoomDatabase.kt
@@ -1,7 +1,36 @@
 package com.droidrbi.wordlist.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.droidrbi.wordlist.model.Word
+import java.security.AccessControlContext
 
+@Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
 public abstract class WordRoomDatabase: RoomDatabase() {
+
+    abstract fun wordDao(): WordDao
+
+    companion object{
+
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase{
+            val tempInstance = INSTANCE
+            if (tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 
 }
